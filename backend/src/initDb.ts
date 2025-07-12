cf8d0d146b772be8737aee88b6159bdd16e93c1e
felixcao <felixcao37@gmail.com>
2025-07-12 20:13:29 +1000

    seeded topics

--- backend/src/initDb.ts
+++ backend/src/initDb.ts
@@ -133,11 +133,11 @@ export const initDB = async () => {
 		INSERT INTO classes (classid, classname) VALUES (2, 'YEAR 10 MATHS');
 		INSERT INTO classes (classid, classname) VALUES (3, 'YEAR 11 MATHS');
 		INSERT INTO classes (classid, classname) VALUES (4, 'YEAR 12 MATHS');
-		INSERT INTO classes (classid, classname) VALUES (5, 'COMP1511 Wednesday');
-		INSERT INTO classes (classid, classname) VALUES (6, 'COMP1511 Thursday');
-		INSERT INTO classes (classid, classname) VALUES (7, 'COMP1521 Monday');
-		INSERT INTO classes (classid, classname) VALUES (8, 'COMP1521 Tuesday');
-		INSERT INTO classes (classid, classname) VALUES (9, 'COMP1531 Friday');
+		INSERT INTO classes (classid, classname) VALUES (5, 'YEAR 7A ENGLISH');
+		INSERT INTO classes (classid, classname) VALUES (6, 'YEAR 7B ENGLISH');
+		INSERT INTO classes (classid, classname) VALUES (7, 'YEAR 8A SCIENCE');
+		INSERT INTO classes (classid, classname) VALUES (8, 'YEAR 8B SCIENCE');
+		INSERT INTO classes (classid, classname) VALUES (9, 'YEAR 8C SCIENCE');
 	`);
 
 	// Insert class teacher relationship
@@ -169,6 +169,39 @@ export const initDB = async () => {
 			)
 		}
 	}
+
+	await db.exec(`
+   	INSERT INTO topics (topicid, classid, topicname) VALUES (1, 1, 'Basic Trigonometry');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (2, 1, 'Quadratic Equations');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (3, 1, 'Coordinate Geometry');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (4, 2, 'Circle Geometry');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (5, 2, 'Further Trigonometry');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (6, 2, 'Non Linear Geometry');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (7, 3, 'Solving Inequalities');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (8, 3, 'Projectile Motion');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (9, 3, 'Differentiation');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (10, 4, 'Integration');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (11, 4, 'Simple Harmonic Equation');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (12, 4, 'Mechanics');
+
+		INSERT INTO topics (topicid, classid, topicname) VALUES (13, 5, 'Creative Writing');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (14, 5, 'Macbeth');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (15, 5, 'Rime of the Ancient Mariner');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (16, 6, 'Creative Writing');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (17, 6, 'Macbeth');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (18, 6, 'Rime of the Ancient Mariner');
+
+		INSERT INTO topics (topicid, classid, topicname) VALUES (19, 7, 'Ecosystems');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (20, 7, 'Cells');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (21, 7, 'Periodic Table');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (22, 8, 'Ecosystems');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (23, 8, 'Cells');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (24, 8, 'Periodic Table');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (25, 9, 'Ecosystems');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (26, 9, 'Cells');
+		INSERT INTO topics (topicid, classid, topicname) VALUES (27, 9, 'Periodic Table');
+	`);
+	
   return db;
 }
 
