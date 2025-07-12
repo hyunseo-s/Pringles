c2a418d8e21167d9ddd6141ff115c779c36dec1f
Elizabeth Zhu Chan <z5416950@ad.unsw.edu.au>
2025-07-12 16:25:12 +1000

    Implemented topics routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import morgan from 'morgan';
 import { initDB } from './initDb';
 import { login, register } from './funcs/auth';
 import { decodeJWT } from './utils';
-// import { addQuestion, createTopics, getStudentTopicData, getTeacherTopicData, getTopics } from './funcs/topics';
+import { addQuestion, createTopics, getStudentTopicData, getTeacherTopicData, getTopics } from './funcs/topics';
 
 // Set up web app
 const app = express();
@@ -114,56 +114,57 @@ app.post('/auth/logout', async (req: Request, res: Response) => {
 // // ====================================================================
 
 
-// app.post('/topics/create', async (req: Request, res: Response) => {
-//   try {
-//     const { topics } = req.body;
-//     const topicId = await createTopics(topics);
-//     res.status(200).json(topicId);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message })
-//   }
-// })
+app.post('/topics/{classId}/create', async (req: Request, res: Response) => {
+  try {
+    const classId = req.params.classId;
+    const { topics } = req.body;
+    const topicId = await createTopics(classId, topics);
+    res.status(200).json(topicId);
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
 
-// app.get('/topics/:classId', (req: Request, res: Response) => {
-//   const classId = parseInt(req.params.classId);
-//   try {
-//     const classes = getTopics(classId);
-//     res.status(200).json(classes);
-//   } catch (error) {
-//     res.status(404).json({ error: error.message });
-//   }
-// });
+app.get('/topics/:classId', (req: Request, res: Response) => {
+  const classId = parseInt(req.params.classId);
+  try {
+    const classes = getTopics(classId);
+    res.status(200).json(classes);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
-// app.post('/topics/:topicId/question', async (req: Request, res: Response) => {
-//   const topicId = parseInt(req.params.topicId);
-//   const { question, level } = req.body;
-//   try {
-//     const questionId = await addQuestion(topicId, level, question);
-//     res.status(200).json(questionId);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
+app.post('/topics/:topicId/question', async (req: Request, res: Response) => {
+  const topicId = parseInt(req.params.topicId);
+  const { question, level } = req.body;
+  try {
+    const questionId = await addQuestion(topicId, level, question);
+    res.status(200).json(questionId);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-// app.get('/topic/:topicId/teacher/data', (req: Request, res: Response) => {
-//   const topicId = parseInt(req.params.topicId);
-//   try {
-//     const topicData = getTeacherTopicData(topicId);
-//     res.status(200).json(topicData);
-//   } catch (error) {
-//     res.status(404).json({ error: error.message });
-//   }
-// });
+app.get('/topic/:topicId/teacher/data', (req: Request, res: Response) => {
+  const topicId = parseInt(req.params.topicId);
+  try {
+    const topicData = getTeacherTopicData(topicId);
+    res.status(200).json(topicData);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
-// app.get('/topic/:topicId/student/data', (req: Request, res: Response) => {
-//   const topicId = parseInt(req.params.topicId);
-//   try {
-//     const topicData = getStudentTopicData(topicId);
-//     res.status(200).json(topicData);
-//   } catch (error) {
-//     res.status(404).json({ error: error.message });
-//   }
-// });
+app.get('/topic/:topicId/student/data', (req: Request, res: Response) => {
+  const topicId = parseInt(req.params.topicId);
+  try {
+    const topicData = getStudentTopicData(topicId);
+    res.status(200).json(topicData);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
 // app.get('/classes/:classId/data', (req: Request, res: Response) => {
 //   const classId = req.params.classId;
