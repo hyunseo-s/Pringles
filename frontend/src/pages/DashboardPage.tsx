5023cc55e81b44c6bcf95aa8ed2cdf56bd76d894
felixcao <felixcao37@gmail.com>
2025-07-12 13:04:29 +1000

    added class and topic modals

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -5,8 +5,14 @@ import { Button, Flex, Paper, Text } from '@mantine/core';
 import { ClassButton } from '../components/dashboard/ClassButton';
 import { StatsRing } from '../components/dashboard/StatRing';
 import { TopicsCarousel } from '../components/dashboard/TopicsCarousel';
+import { useDisclosure } from '@mantine/hooks';
+import { CreateTopicModal } from '../components/dashboard/CreateTopicModal';
+import { CreateClassModal } from '../components/dashboard/CreateClassModal';
 
 const DashboardPage = () => {
+	const [openedTopicModal, { open: openTopicModal, close: closeTopicModal }] = useDisclosure(false);
+	const [openedClassModal, { open: openClassModal, close: closeClassModal }] = useDisclosure(false);
+
 	const navigate = useNavigate();
 	const { user } = useUser();
 
@@ -26,20 +32,23 @@ const DashboardPage = () => {
 
   if (!user) return null; // optional: show a loading spinner here
 
-
   return (
 		<Paper>
 			<div className='mb-4'>
 				<Flex justify='space-between' mb='lg'>
 					<Text size='2rem'>My Classes</Text>
-					<Button>Create</Button>
+					<Button variant="light" onClick={openClassModal}>
+						Create
+					</Button>
+					<CreateClassModal opened={openedClassModal} close={closeClassModal} />
 				</Flex>
 				<Flex gap={24}>
 					{
 						classes.map((c, i) => 
 						<ClassButton 
 							active={classIndex == i} 
-							onClick={() =>setClassIndex(i)}>
+							onClick={() =>setClassIndex(i)}
+						>
 								{c}
 						</ClassButton>)
 					}
@@ -51,7 +60,10 @@ const DashboardPage = () => {
 			</Flex>
 			<Flex justify='space-between' mb='2rem'>
 				<Text size='1.5rem'>Class Topics</Text>
-				<Button>Add New</Button>
+				<Button variant="light" onClick={openTopicModal}>
+					Add New
+				</Button>
+				<CreateTopicModal opened={openedTopicModal} close={closeTopicModal} />
 			</Flex>
 			<TopicsCarousel />
 		</Paper>
