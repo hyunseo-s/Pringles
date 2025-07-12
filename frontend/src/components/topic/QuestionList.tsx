de52f1f3510aff4ae4287996088ef11ac1d2271b
felixcao <felixcao37@gmail.com>
2025-07-12 15:50:11 +1000

    added questions list

---
+++ frontend/src/components/topic/QuestionList.tsx
@@ -0,0 +1,99 @@
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
+import cx from 'clsx';
+import { ActionIcon, Anchor, InputLabel, NumberInput, Text } from '@mantine/core';
+import { useListState } from '@mantine/hooks';
+import classes from './QuestionList.module.css';
+import { useState } from 'react';
+import { IconCross, IconX } from '@tabler/icons-react';
+
+const data = [
+  { position: 6, difficulty: 10, dateCreated: new Date(), symbol: '1', question: 'What is the powerhouse of the cell?' },
+  { position: 7, difficulty: 8, dateCreated: new Date(), symbol: '2', question: 'What are the key differences between prokaryotic and eukaryotic cells?' },
+  { position: 39, difficulty: 3, dateCreated: new Date(), symbol: '3', question: 'Describe the process of protein synthesis, starting from transcription to translation.' },
+];
+
+export function QuestionList() {
+  const [state, handlers] = useListState(data);
+  const items = state.map((item, index) => (
+    <QuestionItem item={item} index={index} state={state} handlers={handlers}/>
+  ));
+
+  return (
+    <DragDropContext
+      onDragEnd={({ destination, source }) =>
+        handlers.reorder({ from: source.index, to: destination?.index || 0 })
+      }
+    >
+      <Droppable droppableId="dnd-list" direction="vertical">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {items}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+const QuestionItem = ({ item, index, state, handlers }) => {
+	const [difficulty, setDifficulty] = useState(item.difficulty);
+
+	return (
+		<Draggable key={item.symbol} index={index} draggableId={item.symbol}>
+      {(provided, snapshot) => (
+        <div
+          className={cx(classes.item, { [classes.itemDragging]: snapshot.isDragging })}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Text className={classes.symbol}>{item.symbol}</Text>
+					<div className='flex justify-between w-full'>
+						<div>
+							<Text size='sm'>{item.question}</Text>
+							<div className='flex gap-2'>
+								<Anchor c="dimmed" size="sm" my='auto' className="hover:underline cursor-pointer">
+									Answered {1 + Math.floor(Math.random() * 99)} times*
+								</Anchor>
+								<Text c="dimmed" size="sm" my='auto'> • </Text>
+								<div className='my-auto flex'>
+									<Text c="dimmed" size="sm" my='auto' mr={'0.25rem'}>Difficulty</Text>
+									<div className='w-10'>
+										<NumberInput
+											className={classes.input}
+											c={"dimmed"}
+											variant="unstyled"
+											value={difficulty}
+											onChange={(e) => setDifficulty(e)}
+											min={1}
+											max={10}
+										/>
+									</div>
+								</div>
+							</div>
+							<Text c="dimmed" size="sm">
+								Created {item.dateCreated.toDateString()}
+							</Text>
+						</div>
+						<div className='flex gap-2'>
+							<div className='ml-8'>
+								<ActionIcon
+									variant="outline"
+									color='gray'
+									size='sm'
+									aria-label="Gradient action icon"
+									onClick={() => {handlers.remove(index)}}
+									// variant="gradient"
+									// gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+								>
+									<IconX />
+								</ActionIcon>
+							</div>
+						</div>
+          </div>
+        </div>
+      )}
+    </Draggable>
+	)
+}
--- frontend/src/components/topic/TeacherTopicView.tsx
+++ frontend/src/components/topic/TeacherTopicView.tsx
@@ -1,18 +1,23 @@
-import { ActionIcon, Text } from "@mantine/core"
+import { ActionIcon, Progress, Text } from "@mantine/core"
 import { IconArrowLeft } from "@tabler/icons-react"
 import type { Topic } from "../../types/Topic"
+import { TeacherStatsGrid } from "./TeacherStatsGrid"
+import { useNavigate } from "react-router"
+import { QuestionList } from "./QuestionList"
 
 interface TopicProps {
 	topic: Topic
 }
 
 export const TeacherTopicView = ({ topic }: TopicProps) => {
+	const navigate = useNavigate();
 	return (
 		<div>
 			<ActionIcon
 				variant="filled"
 				size="lg"
 				aria-label="Gradient action icon"
+				onClick={() => navigate('/dashboard')}
 				// variant="gradient"
 				// gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
 			>
@@ -21,9 +26,40 @@ export const TeacherTopicView = ({ topic }: TopicProps) => {
 			<div className="h-64 w-full bg-black mt-2">
 				Banner
 			</div>
-			<Text size="xl">
+			<p className="text-3xl my-6">
 				{topic.name}
-			</Text>
+			</p>
+			<div className="flex gap-20 mb-10">
+				<div className="flex flex-col">
+					<div className="text-sm">
+						Class Average Score
+					</div>
+					<div className="text-xl text-center my-3">
+						80
+					</div>
+					<Progress value={80} />
+				</div>
+				<div className="flex flex-col">
+					<div className="text-sm ">
+						Class Average Level
+					</div>
+					<div className="text-xl text-center my-3">
+						6
+					</div>
+					<Progress value={6 * 10} />
+				</div>
+			</div>
+			<p className="text-2xl mb-4">
+				Topic Breakdown
+			</p>
+			<p className="text-lg mb-3">
+				Students by Level
+			</p>
+			<TeacherStatsGrid />
+			<p className="text-lg mt-8 mb-6">
+				Students by Question
+			</p>
+			<QuestionList />
 		</div>
 	)
 }
