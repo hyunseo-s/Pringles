4afd0bba18fdb2d43fe8c2695955a0043fc2e612
Ivan Chan <107239075+ivan-chan-1@users.noreply.github.com>
2025-07-12 23:20:54 +1000

    added previous qs

--- frontend/src/components/topic/StudentTopicView.tsx
+++ frontend/src/components/topic/StudentTopicView.tsx
@@ -1,9 +1,10 @@
-import { ActionIcon, Button, Flex, Text } from "@mantine/core"
-import { IconArrowLeft } from "@tabler/icons-react";
+import { ActionIcon, Badge, Button, Divider, Flex, Paper, Text } from "@mantine/core"
+import { IconArrowDown, IconArrowLeft } from "@tabler/icons-react";
 import { useNavigate } from "react-router";
 import type { TopicProps } from "./TeacherTopicView";
 import { useParams } from 'react-router-dom';
 import StudentStats, { type StudentStatsType } from "./StudentStats.tsx";
+import { useRef } from "react";
 
 const studentSnapshot : StudentStatsType[] = [
 	{ title: 'Best Score', icon: 'star', value: '90', diff: 18 },
@@ -12,14 +13,17 @@ const studentSnapshot : StudentStatsType[] = [
 ]
 
 const studentLevels : StudentStatsType[] = [
-	{ title: 'Level 1-2', icon: 'level1', value: '90/100'},
-    { title: 'Level 3-4', icon: 'level2', value: '80/100'},
-    { title: 'Level 5', icon: 'level3', value: '7/100' }
+	{ title: 'Level 1-2 Questions', icon: 'level1', value: '90/100'},
+    { title: 'Level 3-4 Questions', icon: 'level2', value: '80/100'},
+    { title: 'Level 5 Questions', icon: 'level3', value: '7/100' }
 ]
+
+const previousQs = [{ type: "written answer", level: 1, question: "Explain the process of photosynthesis in plants."}, { type: "multiple", level: 2, question: "Which planet is known as the 'Red Planet'?"}];
 
 export const StudentTopicView = ({ topic }: TopicProps) => {
 	const navigate = useNavigate();
 	// const { topicId } = useParams();
+	const targetRef = useRef<HTMLDivElement>(null);
 
 	const handleStart = async () => {
 		// calls /session/{classId}/{topicId}/start
@@ -27,6 +31,10 @@ export const StudentTopicView = ({ topic }: TopicProps) => {
 
 		// navigate(`/quiz/${res.sessionId}`)
 		navigate(`/quiz/0/0`)
+	}
+
+	const scrollToTarget = () => {
+		targetRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
 	}
 
 	return (
@@ -44,11 +52,12 @@ export const StudentTopicView = ({ topic }: TopicProps) => {
 			</div>
 
 			{/* Progress Report */}
-			<Flex align="center" justify='space-between' className="mt-18">
+			<Flex align="center" justify='space-between' className="my-18">
 				<Flex direction="column" gap={"3.5rem"}>
 					<p className="text-6xl">
 						{topic.name}
 					</p>
+
 					<Button radius="xl" w="200" onClick={() => handleStart()}>START PRACTICE</Button>
 				</Flex>
 				<Flex direction="column" gap="md">
@@ -56,17 +65,51 @@ export const StudentTopicView = ({ topic }: TopicProps) => {
 				</Flex>
 			</Flex>
 
+			<Divider	
+				my="xs"
+				label={
+					<Button 
+						variant="subtle" 
+						radius={"xl"} 
+						leftSection={<IconArrowDown size={14}/>}
+						onClick={scrollToTarget}
+					>
+						View Previous Peformance
+					</Button>
+				}
+			/>
+
 			{/* Student Level Report */}
-			<div className="mt-42">
+			<div className="mt-18" ref={targetRef}>
 				<Text fz={"1.5rem"} mb="xl">Level Performance</Text>
 				<StudentStats data={studentLevels}/>
 			</div>
 			
 			{/* Previous Questions */}
-			<div className="mt-30">
+			<div className="mt-18">
 				<Text fz={"1.5rem"} mb="xl">Review Completed Questions</Text>
-			</div>
+				<Flex align={"center"} direction={"column"} gap={"lg"}>
+					{/* Written Answer */}
+					{previousQs.map((q, i) =>  
+						<Paper withBorder p="md" radius="md" w={700}>
+							<Text size="xs" c="dimmed" tt="uppercase" fw={700} mb={10}>
+								Level {q.level}
+							</Text>
+							<Text fw={500}>{q.question}</Text>
 
+							{q.type === "multiple" && (
+								<>
+									<Paper p="xs" radius="md" m={"md"} bg={"gray.1"}>Earth</Paper>
+									<Paper p="xs" radius="md" m={"md"} bg={"gray.1"}>Venus</Paper>
+									<Paper p="xs" radius="md" m={"md"} bg={"gray.1"}>Mars</Paper>
+									<Paper p="xs" radius="md" m={"md"} bg={"gray.1"}>Jupiter</Paper>
+								</>
+							)}	
+						</Paper>
+					)}
+					
+				</Flex>
+			</div>
 
 		</Flex>
 	)
