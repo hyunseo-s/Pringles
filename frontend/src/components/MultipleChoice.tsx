04d1364bb42a6528bc14511fdaaf59152a6ad729
Ivan Chan <107239075+ivan-chan-1@users.noreply.github.com>
2025-07-12 13:52:19 +1000

    multiple choice questions

---
+++ frontend/src/components/MultipleChoice.tsx
@@ -0,0 +1,68 @@
+import { Button, Stack, type DefaultMantineColor } from "@mantine/core";
+import { useState } from "react";
+
+const MultipleChoice = ({ 
+	options, 
+	answer, 
+	correctId 
+} : { 
+	answer: React.Dispatch<React.SetStateAction<string | null>>, 
+	correctId: string | null, 
+	options: {id: string, label: string}[]
+}) => {
+	const [selected, setSelected] = useState<string | null>(null);
+
+	const handleClick = (id: string) => {
+		// Disables clicks after submitting answer
+		if (correctId !== null) {
+			return;
+		}
+
+		if (selected === id) {
+			setSelected(null);
+			answer(null)
+		} else {
+			setSelected(id);
+			answer(id);
+		}
+	};
+
+	const setColours = (id: string) : DefaultMantineColor => {
+		if (correctId === null && selected === id) {
+			return "blue";
+		} else if (correctId !== null && selected === id && id === correctId) {
+			return "green";
+		} else if (correctId !== null && selected === id && id !== correctId) {
+			return "red";
+		}
+
+		return "gray"
+	}
+
+	return (
+		<div>
+			<Stack style={{ width: 500 }} gap={"xs"}>
+				{options.map(({ id, label }) => (
+					<Button variant="light" w={500} color={setColours(id)} justify='flex-start' styles={{ 
+						root: { 
+							'--button-height': 'auto',
+							paddingTop: 'var(--button-padding-x-xs)',
+							paddingBottom: 'var(--button-padding-x-xs)'
+						},
+						label: {
+							whiteSpace: 'normal',
+							fontWeight: selected === id ? 'bold' : 'normal',
+							color: selected === id ? 'var(--button-color)' : '#2e2e2e',
+						}
+					}}
+					onClick={() => handleClick(id)}
+					>
+						{label}
+					</Button>
+				))}
+    		</Stack>
+		</div>
+	)
+}
+
+export default MultipleChoice
--- frontend/src/pages/QuizPage.tsx
+++ frontend/src/pages/QuizPage.tsx
@@ -1,10 +1,32 @@
 import { useNavigate } from 'react-router';
 import { useUser } from '../context/UserContext';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import MultipleChoice from '../components/MultipleChoice';
+import { Button, Flex, Modal, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const options = [
+  { id: "first", label: "First" },
+  { id: "second", label: "Second" },
+  { id: "third", label: "Third" },
+  { id: "fourth", label: "Fourth" }
+];
+
+const options2 = [
+  { id: "fifth", label: "Fifth" },
+  { id: "sixth", label: "Sixth" },
+  { id: "seventh", label: "Seventh" },
+  { id: "eighth", label: "Eighth" }
+];
 
 const QuizPage = () => {
 	const navigate = useNavigate();
 	const { user } = useUser();
+  const [ answer, setAnswer ] = useState<string | null>(null);
+  const [ correct, setCorrect ] = useState<string | null>(null);
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [option, setOption] = useState<boolean>(true); // TO REMOVE
 
   useEffect(() => {
     if (!user) {
@@ -14,10 +36,67 @@ const QuizPage = () => {
 
   if (!user) return null; // optional: show a loading spinner here
 
+  const handleSubmit = () => {
+    const fetchCorrect = () => {
+      return "first";
+    }
+
+    setCorrect(fetchCorrect());
+  }
+
+  const handleNext = () => {
+    setAnswer(null);
+    setCorrect(null);
+    setOption(!option);
+  }
+
+  const handleFinish = () => {
+    navigate("/dashboard");
+  }
+
   return (
-		<>
-		Quiz
-		</>
+		<Flex mih="100vh" maw="100vw" direction={"column"} justify="center" align="center" gap={20}>
+
+      {/* Question */}
+		  <div>What is the longest river in the world, and through which countries does it flow?</div>
+
+      {/* Answer(s) */}
+      <MultipleChoice answer={setAnswer} correctId={correct} options={option ? options : options2}/>
+
+      <Flex direction={'column'} gap={10}>
+        {/* Submit / Next Buttons */}
+        {correct === null
+          ? 
+          <Button 
+            disabled={answer === null} 
+            onClick={() => handleSubmit()}
+          >
+            SUBMIT
+          </Button>
+          :
+          <Button 
+            color={correct === answer ? "green" : "red"} 
+            onClick={() => handleNext()}
+          >
+            NEXT
+          </Button>
+        }
+
+        {/* Finish Button */}
+        <Button variant="light" color="gray" onClick={open}>FINISH</Button>
+      </Flex>
+
+      {/* Confirmation Modal */}
+      <Modal opened={opened} onClose={close} title="Are you sure you want to end the session?" centered>
+        <Flex direction={'column'} gap={20}>
+          <Text size="sm">Your progress will be saved.</Text>
+          <Flex justify={'space-between'}>
+            <Button color="gray" variant='light' onClick={close}>CLOSE</Button>
+            <Button color="red" onClick={handleFinish}>FINISH</Button>
+          </Flex>
+        </Flex>
+      </Modal>
+		</Flex>
   );
 }
 
